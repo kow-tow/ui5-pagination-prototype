47e72ed7f0cbb91b4c685710bb0773598d252e1d
☘ <kogawananari@gmail.com>
2024-10-26 16:39:29 +0800

    :label:

---
+++ src/ui5.d.ts
@@ -0,0 +1,10 @@
+import type { IButton } from "@ui5/webcomponents/dist/Button.js"
+
+// 不太會寫
+/*declare namespace astroHTML {
+    namespace JSX {
+        interface IntrinsicElements extends astroHTML.JSX.IntrinsicElements {
+            "ui5-button": IButton
+        }
+    }
+}*/
