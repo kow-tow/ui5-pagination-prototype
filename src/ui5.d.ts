c02c0ff97fde96673d7be57b5ec3cf7b684d3522
☘ <kogawananari@gmail.com>
2024-10-28 23:44:44 +0800

    :fire:

--- src/ui5.d.ts
+++ src/ui5.d.ts
@@ -1,11 +1,9 @@
 import Button from "@ui5/webcomponents/dist/Button.js"
 
-// 不太會寫
 declare global {
     namespace astroHTML {
         namespace JSX {
-            interface IntrinsicElements
-                extends astroHTML.JSX.IntrinsicElements {
+            interface IntrinsicElements {
                 "ui5-button": Partial<InstanceType<typeof Button>>
             }
         }
